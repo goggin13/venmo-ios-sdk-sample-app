3915f38aef9f82ab921d74d20bcce73b0210d45f
John Graham <johngraham262@gmail.com>
2012-11-16 15:42:06 -0500

    Initial Commit

---
+++ VenmoSDKSampleAppIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VenmoSDKSampleAppIOS
+//
+//  Created by John Graham on 11/16/12.
+//  Copyright (c) 2012 Venmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VenmoSDKSampleAppIOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VenmoSDKSampleAppIOS
+//
+//  Created by John Graham on 11/16/12.
+//  Copyright (c) 2012 Venmo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
